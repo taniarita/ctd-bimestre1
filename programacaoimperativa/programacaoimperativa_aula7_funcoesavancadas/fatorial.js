61563744548bb56d175c4d38ffc336b686235f15
taniarita <taniamendes6523@gmail.com>
2021-06-15 23:44:45 -0300

    descomentar e comentar a função correta

--- programacaoimperativa/programacaoimperativa_aula7_funcoesavancadas/fatorial.js
+++ programacaoimperativa/programacaoimperativa_aula7_funcoesavancadas/fatorial.js
@@ -1,20 +1,20 @@
-function fatorial(num) {
-    let fat = 1;
-    for(i = 1; i <= num; i++) {
-        fat *= i; 
-    }
-    return fat
-}
-console.log(fatorial(7))
+// function fatorial(num) {
+//     let fat = 1;
+//     for(i = 1; i <= num; i++) {
+//         fat *= i; 
+//     }
+//     return fat
+// }
+// console.log(fatorial(7))
 
 
 //com recursividade
-// function fatorial(num) {
-//     if(num < 0) return;
-//     if(num === 0) return 1;
-//     return num * fatorial(num - 1); 
-// }
-// console.log(fatorial(7));
+function fatorial(num) {
+    if(num < 0) return;
+    if(num === 0) return 1;
+    return num * fatorial(num - 1); 
+}
+console.log(fatorial(7));
 
 
 
